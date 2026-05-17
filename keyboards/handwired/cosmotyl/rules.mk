0264e3e0143d3ddf26a6fa2a904e726766555ccb
Aaron V <aaron.vos@gmail.com>
2026-05-17 16:45:19 -0400

    Added cosmotyl

---
+++ keyboards/handwired/cosmotyl/rules.mk
@@ -0,0 +1,16 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+RGBLIGHT_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = no
+SERIAL_DRIVER = vendor
+VIA_ENABLE = yes
+CONSOLE_ENABLE=yes
+
+# Trackball
+#POINTING_DEVICE_ENABLE = yes
+#POINTING_DEVICE_DRIVER = pmw3389 # Change to pmw3360 if you use that
+
